001f2f7f57ff092d0a50e280020fd78491510e2f
gk07 <gk_520@hotmail.com>
2021-06-06 10:14:50 +0800

    Hide keys, ids in config.js and project.config.json

--- config.js
+++ config.js
@@ -1,23 +1,25 @@
-const sheetToken = "shtcnTNuG88jU9y2Ldfp9ZgjjUb";
-const folderToken = "fldcnQ7Qa7RosZTqvzaHveun0og";
+// replace ids, secrets, keys before run code.
+
+const sheetToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const folderToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 var config = {
-  app_id: "cli_9e071c8f77b1d00d",
-  app_secret: "lnIhLD4fMTU467ufS3VAqgDv6eliG2mW",
+  app_id: "xxxxxxxxxxxxxxxxxxxx",
+  app_secret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   sheetIds: {
-    spots: "5a3db1",
-    cars: "pcnBE5",
-    history: "2k97WA",
-    empty_his: "MxrYrB",
-    fake_his: "efOlXV",
-    msgbot_whitelist: "AR7UbN",
-    chat_ids: "v8hpjv",
+    spots: "xxxxxx",
+    cars: "xxxxxx",
+    history: "xxxxxx",
+    empty_his: "xxxxxx",
+    fake_his: "xxxxxx",
+    msgbot_whitelist: "xxxxxx",
+    chat_ids: "xxxxxx",
   },
   fakeMode: false,
   showLoading: true,
   msgBot: true,
   msgReceiver: {
-    chat_id: "oc_1f5ac5bd6569328f5db6819ad207d5bd",
+    chat_id: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   },
 };
 
